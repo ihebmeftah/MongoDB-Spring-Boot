642f16e5aa7a2f0f893a75fac4d5b25734b62620
iheb meftah <ihebmeftah666@gmail.com>
2023-01-15 11:42:29 +0100

    create collection with commandRunner

--- src/main/java/com/cuba/cuba/CubaApplication.java
+++ src/main/java/com/cuba/cuba/CubaApplication.java
@@ -1,7 +1,11 @@
 package com.cuba.cuba;
 
+import com.cuba.cuba.product.Product;
+import com.cuba.cuba.product.ProductRepo;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CubaApplication {
@@ -9,5 +13,15 @@ public class CubaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CubaApplication.class, args);
 	}
+@Bean
+	public CommandLineRunner commandLineRunner(ProductRepo productRepo){
+		return args ->  {
+			var product = Product.builder()
+					.name("IPhone")
+					.description("Smart Phone created by apple")
+					.build();
 
+			productRepo.insert(product);
+		};
+}
 }
