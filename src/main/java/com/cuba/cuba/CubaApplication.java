6d81d1c568c7e03bfd12428adc2a8daa328e7a42
iheb meftah <ihebmeftah666@gmail.com>
2023-01-15 10:47:02 +0100

    Init

---
+++ src/main/java/com/cuba/cuba/CubaApplication.java
@@ -0,0 +1,13 @@
+package com.cuba.cuba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CubaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CubaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cuba/cuba/CubaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cuba.cuba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CubaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
