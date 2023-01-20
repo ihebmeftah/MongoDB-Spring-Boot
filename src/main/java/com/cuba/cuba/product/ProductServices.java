ab3f53f4c666615c3b152b65446d1576db5aaf6c
iheb meftah <ihebmeftah666@gmail.com>
2023-01-20 14:14:48 +0100

    product service crud

---
+++ src/main/java/com/cuba/cuba/product/ProductServices.java
@@ -0,0 +1,21 @@
+package com.cuba.cuba.product;
+
+import java.util.List;
+
+public class ProductServices {
+    public String insertOne(Product product){
+        return  null ;
+    }
+
+    public Product findbyId(String id){
+        return null;
+    }
+
+    public List<Product> findAll(String id){
+        return null;
+    }
+
+    public  void delete (){
+
+    }
+}
