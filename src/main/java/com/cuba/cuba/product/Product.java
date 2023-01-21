a5b2368d2defd2edefcfdc9e097db066b3f82f05
iheb meftah <ihebmeftah666@gmail.com>
2023-01-21 19:09:14 +0100

    add dbref to product category

--- src/main/java/com/cuba/cuba/product/Product.java
+++ src/main/java/com/cuba/cuba/product/Product.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.util.List;
@@ -19,6 +20,7 @@ public class Product {
     private String name;
     private String description;
     private List<String> tags;
+    @DBRef
     private Category category;
 
 }
